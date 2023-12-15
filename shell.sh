bbba3de65fe243ffcc9999664a872f4a130b6be8
juju <julien.megnoux@me.com>
2023-12-15 14:42:31 +0100

    ajout de la 1ère option

--- shell.sh
+++ shell.sh
@@ -15,10 +15,10 @@ else
     echo "Compilation de l'exécutable en C..."
 
     # Spécifiez le chemin du code source C
-    source_path="$HOME/Documents/Projet/progc.c"
+    source_path="$HOME/Documents/Projet/CY_Trucks/progc/progc.c"
 
     # Spécifiez le chemin de sortie de la compilation
-    output_path="$HOME/Documents/Projet/exec"
+    output_path="$HOME/Documents/Projet/CY_Trucks/exec"
 
     # Compilez le code source en un exécutable
     gcc "$source_path" -o "$output_path"
@@ -32,8 +32,7 @@ else
     fi
 fi
 
-# Exécutez l'exécutable en C
-"$executable_path"
+
 
 
 
@@ -87,6 +86,34 @@ else
 fi
 
 
+
+
+
+#option d1
+
+
+
+
+# Utiliser Awk pour extraire les colonnes des prénoms (sixième position) et des numéros de trajet (première position)
+#awk -F ';' 'NR > 1 {gsub(/"/, "", $1); gsub(/"/, "", $6); print $1 ";" $6}' data/data.csv > temp/donnees.txt
+
+# Utiliser Awk pour compter le nombre de trajets par prénom et stocker le résultat dans un fichier temporaire
+#awk -F ';' 'NR > 1 {count_prenom[$1]++; prenom[$1]=$2} END {for (p in prenom) print p ";" count_prenom[p] ";" prenom[p]}' temp/donnees.txt > temp/nombre_trajets_par_prenom.txt
+
+# Trier la liste du nombre de trajets par prénom par ordre décroissant, afficher les 10 premiers, puis trier à nouveau par le nombre de trajets
+#sort -t';' -k2 -n -r temp/nombre_trajets_par_prenom.txt | head -n 10 | sort -t';' -k2 -n -r > temp/nombre_trajets_par_prenom_tries.txt
+
+
+
+
+
+
+
+
+
+
+
+
 # Enregistrez le temps de fin
 end_time=$(date +%s)
 
@@ -95,7 +122,4 @@ execution_time=$((end_time - start_time))
 
 # Affichez le temps d'exécution
 echo "Le script a mis $execution_time secondes à s'exécuter."
-
-
-# Reste du script...
 
