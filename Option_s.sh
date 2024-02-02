c257809f38b50e195a7a63b30783551ddf87b541
kanthasi <105271457+kanthasi@users.noreply.github.com>
2024-02-02 18:29:32 +0100

    Update Option_s.sh

--- Option_s.sh
+++ Option_s.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cat data/data.csv |awk -F ';' 'NR > 1 {gsub(/"/, "", $1); gsub(/"/, "", $5); print $1 ";" $5}'>resultat.txt
+cat data/data.csv |awk -F ';' 'NR > 1 {gsub(/"/, "", $1); gsub(/"/, "", $5); print $1 ";" $5}'> resultat.txt
 gcc -o exec progc/Traitement_s/main.c
 ./exec | head -50 > temp/vamos.txt
 gnuplot gnuplot_s.gp
